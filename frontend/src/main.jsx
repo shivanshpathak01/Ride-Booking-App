227c0c032cdb5a54f62287e777ab5a2f9eed86e9
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-01-05 00:06:09 +0530

    first commit for main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
