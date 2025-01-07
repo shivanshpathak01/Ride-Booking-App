d88dd879fc0663897c4adfeebd45f819b9a8840c
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-01-08 00:20:17 +0530

    Location search Panel done

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      {/* this is just a sample data */}
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sultanpur</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sultanpur</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sultanpur</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sultanpur</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>24B, Near Kapoor's cafe, Sultanpur</h4>
+      </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
