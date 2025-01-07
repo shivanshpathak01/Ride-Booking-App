2604756234a683c7a8c44934f67bfaf45891365d
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-01-08 00:22:23 +0530

    Captain Protect Wrapper Updated

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,7 +1,7 @@
 import React, {useContext, useEffect, useState} from 'react'
 import { CaptainDataContext } from '../context/CaptainContext'
 import { useNavigate } from 'react-router-dom'
-
+import axios from 'axios'
 
 const CaptainProtectWrapper = ({
     children
